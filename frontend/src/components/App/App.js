aaee198a1dba682f8ced2d83cc1414b3b89588fa
sofiakotsan <sofiakotsan9@gmail.com>
2024-03-27 18:41:05 -0400

    initial commit

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,14 @@
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import '../../assets/theme/style.css';
+import '../../assets/css/style.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello world!
+    </div>
+  );
+}
+
+export default App;
