2c45d3830242b687206fc163e93f64f7450adb67
skotsan <skotsan@opex.com>
2024-03-28 09:37:50 -0400

    added offer secion on the home page

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,7 +1,21 @@
+import { NavLink } from 'react-router-dom';
+import { ReactComponent as OfferImg } from "../../assets/svg/offer-img.svg";
+
 function HomePage() {
     return (
         <div className="container">
-            Головна сторінка
+            <section className="offer-section">
+                <div className="row">
+                    <div className="col-12 col-md-6 d-flex flex-column justify-content-center align-items-start">
+                        <h1>Lorem ipsum dolor sit amet, consectetur</h1>
+                        <p>Maecenas sagittis felis at accumsan sodales. Nunc viverra pulvinar ipsum, convallis blandit lacus congue id.</p>
+                        <NavLink className="btn btn-primary" to="/">Розпочати</NavLink>
+                    </div>
+                    <div className="col-12 col-md-6 mt-5 mt-md-0">
+                        <OfferImg className="offer-img"/>
+                    </div>
+                </div>
+            </section>
         </div>
     );
   }
