185b308765ac38392a21fdf1b9200e059451ece8
skotsan <skotsan@opex.com>
2024-04-02 13:02:51 -0400

    added TagService

---
+++ frontend/src/services/TagService.js
@@ -0,0 +1,15 @@
+const TagService = {
+    getTags() { // GET /tags
+
+    },
+
+    createTag() { // PUT /tags/{name}
+
+    },
+
+    deleteTag() { // DELETE /tags/{name}
+
+    },
+};
+
+export default TagService;
