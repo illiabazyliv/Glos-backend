e20934a4b7113c08e5086c0c8454af1271a3079f
skotsan <skotsan@opex.com>
2024-04-02 12:53:52 -0400

    outlined methods for UserService and GroupService

---
+++ frontend/src/services/GroupService.js
@@ -0,0 +1,23 @@
+const GroupService = {
+    getUserGroups() { // GET /users/{username}/groups
+
+    },
+
+    getGroup() { // GET /users/{username}/groups/{groupName}
+
+    },
+
+    createGroup() { // PUT /users/{username}/group/{groupName}
+
+    },
+
+    updateGroup() { // PATCH /users/{username}/groups/{groupName}
+
+    },
+
+    deleteGroup() { // DELETE /users/{username}/groups/{groupName}
+
+    },
+};
+
+export default GroupService;
--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,6 +1,29 @@
 const UserService = {
-    // /users/{username}
-    getUser() {
+    getUser() { // GET /users/{username}
+
+    },
+
+    getUserImage() { // GET /users/{username}/image
+
+    },
+
+    setUserImage() { // PUT /users/{username}/image
+
+    },
+
+    updateUser() { // PUT /users/{username}
+
+    },
+
+    blockUser() { // POST /users/{username}/block
+
+    },
+
+    enableUser() { // POST /users/{username}/enable
+
+    },
+
+    searchUsers() { // GET /users?login=<username/email/phoneNumber>&page=0&size=10&size="username,asc"
 
     },
 };
