c557b743eb22035448d7bde5b70da6ca521cd1c6
skotsan <skotsan@opex.com>
2024-04-02 13:41:30 -0400

    added CommentSerivce

---
+++ frontend/src/services/CommentService.js
@@ -0,0 +1,27 @@
+const CommentService = {
+    getRepositoryComments() { // GET /users/{username}/repositories/{id}/comments
+
+    },
+
+    addRepositoryComment() { // POST /users/{username}/repositories/{id}/comments
+
+    },
+
+    getFileComments() { // GET /users/{username}/files/{filename}/comments
+
+    },
+
+    addFileComment() { // POST /users/{username}/files/{filename}/comments
+
+    },
+
+    updateComment() { // PUT /comments/{id}
+
+    },
+
+    deleteComment() { // DELETE /comments/{id}
+
+    },
+};
+
+export default CommentService;
