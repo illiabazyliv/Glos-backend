e67a094e4a3b26e799aebf64e3bfde62f424efc2
skotsan <skotsan@opex.com>
2024-04-02 12:56:49 -0400

    added FriendService

---
+++ frontend/src/services/FriendService.js
@@ -0,0 +1,15 @@
+const FriendService = {
+    getUserFriends() { // GET /users/{username}/friends
+
+    },
+
+    addFriend() { // PUT /users/{username}/friends/{username}
+
+    },
+
+    deleteFriend() { // DELETE /users/{username}/friends/{username}
+
+    },
+};
+
+export default FriendService;
