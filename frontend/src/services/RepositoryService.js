6d0e25f2ed75b17ec06b967b4b1c7320ba02c8f3
skotsan <skotsan@opex.com>
2024-04-02 13:00:51 -0400

    added RepositoryService

---
+++ frontend/src/services/RepositoryService.js
@@ -0,0 +1,15 @@
+const RepositoryService = {
+    setRepositoryAccess() { // PUT /users/{username}/repositories/{id}/access
+
+    },
+
+    setRepositoryAccessForUser() { // PUT /users/{username}/repositories/{id}/access/{username}
+
+    },
+
+    setFileAccessForUser() { // POST /users/{username}/repositories/{id}/files/{filename}/access/{username}
+    
+    },
+};
+
+export default RepositoryService;
