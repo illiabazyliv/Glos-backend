2adbd29533f879573c035a50fc9824b735b0fac0
skotsan <skotsan@opex.com>
2024-04-04 10:25:40 -0400

    implemeted FileService methods

--- frontend/src/services/FileService.js
+++ frontend/src/services/FileService.js
@@ -1,34 +1,62 @@
+import { httpInstance } from "../http/httpInstance";
+import { handleHttpError } from "../helpers/helpers";
+
 const FileService = {
-    setFileAccessForUser() { // POST /users/{username}/repositories/{id}/files/{filename}/access/{username}
-    
+    async setFileAccessForUser(username, repositoryId, filename, targetUsername, accessType) { // POST /users/{username}/repositories/{id}/files/{filename}/access/{username}
+        return await httpInstance.put(`/users/${username}/repositories/${repositoryId}/files/${filename}/access/${targetUsername}`, {accessType})
+        .then(function (response) {
+            return response.data;
+        })
+        .catch(error => handleHttpError(error))
     },
 
-    getFile() { // GET /users/{username}/files/{filename}/download
-
+    async getFile(username, filename) { // GET /users/{username}/files/{filename}/download
+        return await httpInstance.get(`/users/${username}/files/${filename}/download`)
+        .then(function (response) {
+            return response.data;
+        })
+        .catch(error => handleHttpError(error))
     },
 
-    downloadFile() {
+    async downloadFile() {
         // will get info from getFile() and download file through SFTP
     },
 
-    getFileInfo() { // GET /users/{username}/files/{filename}
-
+    async getFileInfo(username, filename) { // GET /users/{username}/files/{filename}
+        return await httpInstance.get(`/users/${username}/files/${filename}`)
+        .then(function (response) {
+            return response.data;
+        })
+        .catch(error => handleHttpError(error))
     },
 
-    getFileSharedToken() { // GET /users/{username}/files/{filename}/shared-token
-
+    async getFileSharedToken(username, filename) { // GET /users/{username}/files/{filename}/shared-token
+        return await httpInstance.get(`/users/${username}/files/${filename}/shared-token`)
+        .then(function (response) {
+            return response.data;
+        })
+        .catch(error => handleHttpError(error))
     },
 
-    uploadFile() { // PUT /users/{username}/files/{filename}/upload
-
+    async uploadFile() { // PUT /users/{username}/files/{filename}/upload
+        // waiting for final request body example
+        // todo: also ask how can a file be uploaded if no filename exists yet
     },
 
-    moveFile() { // POST /users/{username}/files/{filename}/files/move
-
+    async moveFile(username, filename, from, to) { // POST /users/{username}/files/{filename}/files/move
+        return await httpInstance.post(`/users/${username}/files/${filename}/files/`, {from, to})
+        .then(function (response) {
+            return response.data;
+        })
+        .catch(error => handleHttpError(error))
     },
 
-    deleteFile() { // DELETE /users/{username}/files/{id}/file/{filename}
-
+    async deleteFile(username, id, filename) { // DELETE /users/{username}/files/{id}/file/{filename}
+        return await httpInstance.delete(`/users/${username}/files/${id}/file/${filename}`)
+        .then(function (response) {
+            return response;
+        })
+        .catch(error => handleHttpError(error))
     },
 
 };
