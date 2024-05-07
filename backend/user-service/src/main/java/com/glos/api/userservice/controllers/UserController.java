93f8e65d6484af16c61ab8243ff366d20084d132
yablonovskydima <yablonovskydmutro1@gmail.com>
2024-05-07 23:50:42 +0300

    Added controller and controller advice

--- backend/user-service/src/main/java/com/glos/api/userservice/controllers/UserController.java
+++ backend/user-service/src/main/java/com/glos/api/userservice/controllers/UserController.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,6 +32,45 @@ public class UserController
     public ResponseEntity<User> getById(@PathVariable Long id)
     {
         return userAPIClient.getById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user)
+    {
+        return userAPIClient.create(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id)
+    {
+        return userAPIClient.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody User newUser)
+    {
+
+        return userAPIClient.updateUser(id, newUser);
+    }
+
+    @GetMapping("/username/{username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable String username)
+    {
+        return userAPIClient.getUserByUsername(username);
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email)
+    {
+        return userAPIClient.getUserByEmail(email);
+    }
+
+    @GetMapping("/phone-number/{phoneNumber}")
+    public ResponseEntity<User> getUserByPhoneNumber(@PathVariable String phoneNumber)
+    {
+        ResponseEntity<User> user = userAPIClient.getUserByPhoneNumber(phoneNumber);
+        System.out.println(user.getStatusCode().is2xxSuccessful());
+        return user;
     }
 
     @GetMapping
