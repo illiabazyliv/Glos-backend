3010f1637246ae9ea82282f504fdb5db3d94c040
Chiaok <bazylivillya@gmail.com>
2024-05-08 01:10:23 +0300

    wrote CommentServiceApplicationTest

--- backend/comment-service/src/test/java/com/glos/commentservice/CommentServiceApplicationTests.java
+++ backend/comment-service/src/test/java/com/glos/commentservice/CommentServiceApplicationTests.java
@@ -1,13 +1,54 @@
 package com.glos.commentservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.glos.commentservice.domain.DTO.UserDTO;
+import com.glos.commentservice.domain.client.ExternalCommentApi;
+import com.glos.commentservice.domain.entities.Comment;
+import com.glos.commentservice.domain.entities.User;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(CommentServiceApplicationTests.class)
+@AutoConfigureMockMvc
 class CommentServiceApplicationTests {
-
+	@Autowired
+	private MockMvc mockMvc;
+	@MockBean
+	private ExternalCommentApi externalCommentApi;
 	@Test
-	void contextLoads() {
+	void contextLoadsTest() throws Exception{
+		Long id = 1L;
+		User user = new User();
+		user.setId(id);
+		user.setUsername("asdf");
+
+		Comment comment = new Comment();
+		comment.setAuthor(user);
+		comment.setText("Test Text");
+
+		Comment created = new Comment();
+		created.setAuthor(user);
+		created.setText("Test Text");
+
+		when(externalCommentApi.create(Mockito.any(Comment.class))).thenReturn(ResponseEntity.ok(created));
+
+		mockMvc.perform(MockMvcRequestBuilders
+						.post("/comments")
+						.content(new ObjectMapper().writeValueAsString(comment))
+						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON));
 	}
 
 }
