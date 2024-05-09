bd2dfddb0c70624b809387ea45e4f8ba279f884a
MykolaMelnyk28 <lemone681@gmail.com>
2024-05-10 00:40:34 +0300

    Create config files

---
+++ backend/auth-service/src/main/java/com/glos/api/authservice/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.glos.api.authservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(x -> {
+            x.requestMatchers("/**").permitAll();
+        });
+        http.httpBasic(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+}
