bbf38c3b70c12b2e0b63e0e7f07eba512d978308
yablonovskydima <yablonovskydmutro1@gmail.com>
2024-05-17 00:46:23 +0300

    Init project

---
+++ backend/access-service/src/main/java/com/glos/accessservice/AccessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.glos.accessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessServiceApplication.class, args);
+	}
+
+}
