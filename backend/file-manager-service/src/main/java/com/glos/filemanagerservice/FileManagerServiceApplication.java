d03579236fdb7fb9855bf2125673bc272b21a6eb
yablonovskydima <yablonovskydmutro1@gmail.com>
2024-05-16 18:56:06 +0300

    initialized project

---
+++ backend/file-manager-service/src/main/java/com/glos/filemanagerservice/FileManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.glos.filemanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagerServiceApplication.class, args);
+	}
+
+}
