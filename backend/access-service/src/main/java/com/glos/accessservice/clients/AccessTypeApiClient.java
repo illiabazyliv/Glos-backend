24b511e62b247e8098d429f06652560fcbf1109c
yablonovskydima <yablonovskydmutro1@gmail.com>
2024-05-17 19:31:09 +0300

    Fixed 503 error added filter search

--- backend/access-service/src/main/java/com/glos/accessservice/clients/AccessTypeApiClient.java
+++ backend/access-service/src/main/java/com/glos/accessservice/clients/AccessTypeApiClient.java
@@ -1,27 +1,33 @@
 package com.glos.accessservice.clients;
 
+import com.glos.accessservice.responseDTO.Page;
 import com.glos.api.entities.AccessType;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
 
-@FeignClient(name = "access-types")
+import java.util.Map;
+
+@FeignClient(name = "accessTypes")
 public interface AccessTypeApiClient
 {
     @GetMapping("/{id}")
-    public ResponseEntity<AccessType> getById(@PathVariable Long id);
+    ResponseEntity<AccessType> getById(@PathVariable Long id);
 
     @GetMapping("/name/{name}")
-    public ResponseEntity<AccessType> getByName(@PathVariable String name);
+    ResponseEntity<AccessType> getByName(@PathVariable String name);
 
     @PostMapping
-    public ResponseEntity<AccessType> create(@RequestBody AccessType request);
+    ResponseEntity<AccessType> create(@RequestBody AccessType request);
 
     @PutMapping("/{id}")
-    public ResponseEntity<AccessType> update(@PathVariable Long id, @RequestBody AccessType request);
+    ResponseEntity<AccessType> update(@PathVariable Long id, @RequestBody AccessType request);
 
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteById(@PathVariable Long id);
+    ResponseEntity<?> deleteById(@PathVariable Long id);
+
+    @GetMapping
+    ResponseEntity<Page<AccessType>> getByFilter(@SpringQueryMap Map<String, Object> filter);
 }
--- backend/access-service/src/main/java/com/glos/accessservice/controllers/AccessTypeController.java
+++ backend/access-service/src/main/java/com/glos/accessservice/controllers/AccessTypeController.java
@@ -1,10 +1,15 @@
 package com.glos.accessservice.controllers;
 
 import com.glos.accessservice.clients.AccessTypeApiClient;
+import com.glos.accessservice.responseDTO.AccessTypesRequestFilter;
+import com.glos.accessservice.responseDTO.Page;
+import com.glos.accessservice.utils.MapUtils;
 import com.glos.api.entities.AccessType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/access-types")
@@ -34,7 +39,7 @@ public class AccessTypeController
     }
 
     @GetMapping("/name/{name}")
-    public ResponseEntity<AccessType> getById(@PathVariable String name)
+    public ResponseEntity<AccessType> getByName(@PathVariable String name)
     {
         return ResponseEntity.ok(accessTypeApiClient.getByName(name).getBody());
     }
@@ -52,5 +57,18 @@ public class AccessTypeController
     {
         accessTypeApiClient.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<AccessType>> getByFilter(@ModelAttribute AccessTypesRequestFilter filter,
+                                                        @RequestParam(name = "page", required = false, defaultValue = "0") int page,
+                                                        @RequestParam(name = "size", required = false, defaultValue = "10") int size,
+                                                        @RequestParam(name = "sort", required = false, defaultValue = "id,asc") String sort)
+    {
+        filter.setPage(page);
+        filter.setSize(size);
+        filter.setSort(sort);
+        Map<String, Object> map = MapUtils.map(filter);
+        return ResponseEntity.ok(accessTypeApiClient.getByFilter(map).getBody());
     }
 }
