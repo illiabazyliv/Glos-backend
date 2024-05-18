a852e7e85d5a4502cd28cde8d64f6ecb60dc142d
MykolaMelnyk28 <lemone681@gmail.com>
2024-05-18 12:19:04 +0300

    Create sys user for internal using

--- backend/database-api-service/src/main/resources/db/migration/V2__default.sql
+++ backend/database-api-service/src/main/resources/db/migration/V2__default.sql
@@ -1,6 +1,6 @@
 USE glos_database;
 
-INSERT INTO access_types(id, name)
+INSERT IGNORE INTO access_types(id, name)
 VALUES
 (1, 'PRIVATE_RW'),
 (2, 'PROTECTED_R'),
@@ -8,7 +8,16 @@ VALUES
 (4, 'PUBLIC_R'),
 (5, 'PUBLIC_RW');
 
-INSERT INTO roles(id, name)
+INSERT IGNORE INTO roles(id, name)
 VALUES
 (1, 'ROLE_ADMIN'),
-(2, 'ROLE_USER');
+(2, 'ROLE_USER'),
+(3, 'ROLE_SYS');
+
+INSERT IGNORE INTO users(id, username, email, password_hash)
+VALUES
+(1, 'sys', 'global.storage.info@gmail.com', '$2a$12$J/qxhK3mIwYdAfHWGLEPEePWpOpMgB/zwFfWhW8BhFqbiQ4xo0Tbi');
+
+INSERT IGNORE INTO users_roles(id, user_id, role_id)
+VALUES
+(1, 1, 3);
