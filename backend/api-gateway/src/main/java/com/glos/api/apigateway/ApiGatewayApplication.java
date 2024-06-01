e9b711a18823477a25d42092e77aa9dd24982600
MykolaMelnyk28 <lemone681@gmail.com>
2024-06-01 20:49:35 +0300

    Assign origin pattern for CORS: 'http://*:3000', 'https://*:3000'

--- backend/api-gateway/src/main/java/com/glos/api/apigateway/ApiGatewayApplication.java
+++ backend/api-gateway/src/main/java/com/glos/api/apigateway/ApiGatewayApplication.java
@@ -3,9 +3,11 @@ package com.glos.api.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableWebFlux
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
