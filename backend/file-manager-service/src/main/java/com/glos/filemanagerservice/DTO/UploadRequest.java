503550d5511a8cc820a8c025ac51efa396db38fc
yablonovskydima <yablonovskydmutro1@gmail.com>
2024-06-06 23:53:07 +0300

    Changed methods in file storage service and changed logic of uploading files in file manager

--- backend/file-manager-service/src/main/java/com/glos/filemanagerservice/DTO/UploadRequest.java
+++ backend/file-manager-service/src/main/java/com/glos/filemanagerservice/DTO/UploadRequest.java
@@ -1,4 +1,5 @@
 package com.glos.filemanagerservice.DTO;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,19 +20,5 @@ public class UploadRequest {
 
     public void setFiles(List<ByteArrayWithPath> files) {
         this.files = files;
-    }
-
-    public Map<String, Object> toMap()
-    {
-        Map<String, Object> map = new HashMap<>();
-
-        for (int i = 0; i < files.size(); i++)
-        {
-            ByteArrayWithPath byteArrayWithPath = files.get(i);
-            map.put("files[" + i + "].filePath", byteArrayWithPath.getFilePath());
-            map.put("files[" + i + "].file", byteArrayWithPath.getFile());
-            map.put("files[" + i + "].contentType", byteArrayWithPath.getContentType());
-        }
-        return map;
     }
 }
--- backend/file-manager-service/src/main/java/com/glos/filemanagerservice/clients/FileStorageClient.java
+++ backend/file-manager-service/src/main/java/com/glos/filemanagerservice/clients/FileStorageClient.java
@@ -1,24 +1,29 @@
 package com.glos.filemanagerservice.clients;
 
 import com.glos.filemanagerservice.DTO.*;
+import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 import java.util.Map;
 
 @FeignClient(name = "fileStorage")
 public interface FileStorageClient
 {
-    @PostMapping("/upload")
-    ResponseEntity<List<FileAndStatus>> uploadFiles(@SpringQueryMap Map<String, Object> request);
-    @GetMapping("/download")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    ResponseEntity<FileAndStatus> uploadFiles(@RequestPart(value = "filePath") String filePath, @RequestPart(value = "file") MultipartFile file);
+    @GetMapping("/files/download")
     ResponseEntity<ByteArrayResource> downloadFile(@RequestBody DownloadRequest request);
 
-    @PutMapping("/update")
+    @PutMapping(value = "/update", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     ResponseEntity<FileAndStatus> updateFile(@ModelAttribute FileWithPath request);
 
     @PostMapping("/move")
